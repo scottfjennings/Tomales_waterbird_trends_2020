d426931a620223031fd15314a28d1f7b34eade84
scottfjennings <scott.f.jennings@gmail.com>
2021-12-09 10:43:26 -0800

    initial commit

---
+++ code/analysis_utilities.R
@@ -0,0 +1,9 @@
+
+
+
+dabbler_goose_swan_genera <- c("Dendrocygna", "Anser", "Branta", "Cygnus", "Aix", "Spatula", "Mareca", "Anas")
+diver_seaduck_genera <- c("Aythya", "Somateria", "Melanitta", "Clangula", "Bucephala", "Oxyura")
+merg_loon_grebe_taxa <- c("Lophodytes", "Mergus", "Podicipediformes", "Gaviiformes")
+corm_peli_taxa <- c("Suliformes", "Pelecaniformes")
+
+fish_eater_taxa <- c("Lophodytes", "Mergus", "Podicipediformes", "Gaviiformes", "Suliformes", "Pelecaniformes")
