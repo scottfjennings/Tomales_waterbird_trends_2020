464c2139c40bb7d7acb56b3d7e4810c1e4da1151
scottfjennings <scott.f.jennings@gmail.com>
2023-02-21 10:04:39 -0800

    add filter_observed_x_years

--- code/analysis_utilities.R
+++ code/analysis_utilities.R
@@ -7,3 +7,26 @@ merg_loon_grebe_taxa <- c("Lophodytes", "Mergus", "Podicipediformes", "Gaviiform
 corm_peli_taxa <- c("Suliformes", "Pelecaniformes")
 
 fish_eater_taxa <- c("Lophodytes", "Mergus", "Podicipediformes", "Gaviiformes", "Suliformes", "Pelecaniformes")
+
+
+
+#' filter_observed_x_years
+#' 
+#' Filter waterbird data to species seen in at least x_years years. 
+#'
+#' @param df data frame with at least alpha.code and study.year
+#' @param x_years number of years observed for cutoff
+#'
+#' @return
+#' @export
+#'
+#' @examples
+filter_observed_x_years <- function(df, x_years) {
+  df2 <- df %>% 
+  distinct(study.year, alpha.code) %>% 
+  count(alpha.code) %>% 
+  filter(n >= x_years) %>% 
+  select(alpha.code) %>% 
+  left_join(df)
+return(df2)
+}
