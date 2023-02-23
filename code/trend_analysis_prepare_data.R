8d7c5344bc2ae240926d860b8c5dfe902fee9ae0
scottfjennings <scott.f.jennings@gmail.com>
2023-02-23 08:09:45 -0800

    initial commit. mostly new files from splitting the old trend analysis file

---
+++ code/trend_analysis_prepare_data.R
@@ -0,0 +1,87 @@
+
+
+library(tidyverse)
+library(here)
+library(birdnames)
+
+source("C:/Users/scott.jennings/Documents/Projects/core_monitoring_research/water_birds/ACR_waterbird_data_management/code/utils.r")
+source("C:/Users/scott.jennings/Documents/Projects/core_monitoring_research/water_birds/waterbird_data_work/code/utility/waterbird_utility_functions.R")
+
+custom_bird_list <- readRDS("C:/Users/scott.jennings/Documents/Projects/my_R_general/birdnames_support/data/custom_bird_list")
+
+
+wbird_keep_taxa <- c("AMCOGRSCLESCBUFF", "AMCO", "COGA", "Anseriformes", "Alcidae", "Gaviidae", "Pelecanidae", "Podicipediformes", "Sterninae", "Suliformes")
+
+options(scipen = 999)
+# data prep ----
+# several dates had data quirks either in the field or on data sheets and should be excluded from analysis
+exclude_dates <- as.Date(c("2011-12-17", # Bivalve count incomplete; ponds only
+                   "2006-02-11", # WCD not surveyed due to fog
+                   "2004-12-18", # inverness, millerton not surveyed
+                   "1999-12-18", # survey done north to south
+                   "1999-01-30", # strong wind, not entered in raw tallies but including here for completeness
+                   #"1996-12-21", # no WCD precount, never many birds there so maybe ok to include. Also multiple data sheet ambiguities, entered raw tally data likely ok but difficult to be sure
+                   "1997-12-20", # apparent large swell and strong wind in north bay
+                   #"1993-02-06", # no CGP precount, never many birds there so maybe ok to include
+                   #"1993-01-23", # no WCD precount, never many birds there so maybe ok to include.
+                   "1992-01-11", # survey apparently done north to south
+                   "1991-12-14", # substantial ambiguity in precount data sheets. only millerton, wcd entered
+                   "1990-12-15", # unclear labeling of section/transect on data sheets and can't reverse engineer. Data not fully entered; date should be excluded from analysis
+                   "1989-12-16" # funky protocol. No precounts. Should probably exclude from analysis 
+                   ))
+
+
+spp_day_total <- readRDS("C:/Users/scott.jennings/Documents/Projects/core_monitoring_research/water_birds/ACR_waterbird_data_management/data_files/working_rds/new_neg_machine_bay_total") %>% 
+  wbird_add_study_day() %>% # from waterbird_utility_functions.R
+  filter(!date %in% exclude_dates, study.year > 1991) %>% 
+  bird_taxa_filter(wbird_keep_taxa) %>% 
+  mutate(alpha.code = ifelse(alpha.code %in% c("GRSC", "LESC"), "SCAUP", alpha.code))
+  
+
+spp_annual <- spp_day_total %>%
+  group_by(study.year, date) %>% 
+  summarise(bay.total = sum(bay.total)) %>% 
+  mutate(alpha.code = "ALL") %>% 
+  bind_rows(spp_day_total %>% dplyr::select(study.year, date, alpha.code, bay.total)) %>% 
+  group_by(study.year, alpha.code) %>% 
+  summarise(p75.abund = floor(quantile(bay.total, 0.75)))
+
+filter(spp_annual, alpha.code == "HEGR") %>% view()
+
+trend_spp <- spp_annual %>% 
+  group_by(alpha.code) %>% 
+  summarise(num.years.detected = n(),
+            median.p75 = median(p75.abund),
+            min.p75 = min(p75.abund),
+            max.p75 = max(p75.abund)) %>% 
+  filter(num.years.detected >= 20 & median.p75 >= 5, alpha.code != "HEGR")
+
+
+saveRDS(trend_spp, here("data_files/trend_spp"))
+
+
+# fill in 0 for years spp not detected
+spp_annual_full <- spp_annual %>%
+  filter(alpha.code %in% trend_spp$alpha.code) %>% 
+  pivot_wider(id_cols = alpha.code, names_from = study.year, values_from = p75.abund) %>% 
+  pivot_longer(-alpha.code, names_to = "study.year", values_to = "p75.abund") %>% 
+  mutate(p75.abund = ifelse(is.na(p75.abund), 0, p75.abund),
+         study.year = as.numeric(study.year))
+
+
+# join bird data with predictors
+spp_annual_full_preds <- spp_annual_full %>% 
+  full_join(., readRDS(here("data_files/predictors"))) %>% # predictor variables
+  rename(moci = mean.moci,
+         fresh = annual.freshwater) %>% 
+  data.frame()
+
+
+readRDS(here("data_files/predictors")) %>% 
+  dplyr::select("fresh" = annual.freshwater, "moci" = mean.moci) %>% 
+  cor()
+
+
+saveRDS(spp_annual_full_preds, here("data_files/spp_annual_full_preds"))
+
+
