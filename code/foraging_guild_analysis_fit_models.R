d90c3aa89328f704108d2966ca0e3bb503d677ae
scottfjennings <scott.f.jennings@gmail.com>
2023-03-13 16:10:45 -0700

    using simple fixed effect only models

--- code/foraging_guild_analysis_fit_models.R
+++ code/foraging_guild_analysis_fit_models.R
@@ -1,32 +1,107 @@
 
 
 library(tidyverse)
+library(here)
+library(MASS) # for glm.nb
+library(AICcmodavg)
+library(lmtest) # for lrtest
 library(birdnames)
 
+custom_bird_list <- readRDS("C:/Users/scott.jennings/Documents/Projects/my_R_general/birdnames_support/data/custom_bird_list")
 
-source("C:/Users/scott.jennings/Documents/Projects/water_birds/ACR_waterbird_data_management/code/utility/waterbird_utility_functions.r")
+trend_spp <- readRDS(here("data_files/trend_spp"))
+
+# fit models to each species ----
+
+p75_annual <- readRDS(here("data_files/spp_annual_full_preds"))
 
 
-wbirds4analysis <- readRDS("C:/Users/scott.jennings/Documents/Projects/water_birds/ACR_waterbird_data_management/data_files/working_rds/wbirds4analysis") %>% 
-  wbird_add_study_day()
+guilds <- read.csv(here("data_files/Foraging guild table.csv")) %>% 
+  mutate(alpha.code = translate_bird_names(common.name, "common.name", "alpha.code")) %>% 
+  dplyr::select(alpha.code, guild)
 
-num_years_detected <- readRDS("C:/Users/scott.jennings/Documents/Projects/water_birds/ACR_waterbird_data_management/data_files/working_rds/num_years_detected")
 
-
-annual_percent_change <- wbirds4analysis %>% 
-  filter(alpha.code %in% filter(num_years_detected, num.year.detected >=20)$alpha.code) %>% 
-  group_by(study.year, alpha.code, section) %>% 
-  summarise(annual.section.spp.mean = mean(section.final)) %>% 
+abund_guilds <- inner_join(p75_annual, guilds) %>% 
+  group_by(guild, study.year, moci, fresh) %>% 
+  summarise(guild.total = sum(p75.abund)) %>% 
   ungroup() %>% 
-  pivot_wider(id_cols = c("alpha.code", "section"), names_from = study.year, values_from = annual.section.spp.mean) %>% 
-  pivot_longer(-c("alpha.code", "section"), names_to = "study.year", values_to = "annual.section.spp.mean") %>%
-  arrange(alpha.code, section, study.year) %>%  
-  group_by(alpha.code, section) %>% 
-  mutate(year.section.percent.change = annual.section.spp.mean - (annual.section.spp.mean/lag(annual.section.spp.mean)))
+  filter(guild != "Omnivore") %>% 
+  droplevels()
+
+guild_n_spp <- inner_join(p75_annual, guilds) %>%
+  distinct(guild, alpha.code) %>% 
+  group_by(guild) %>% 
+  summarise(n.spp = n()) %>% 
+  filter(guild != "Omnivore")
+  
 
 
+year2_fresh_moci <- glm.nb(guild.total ~ poly(study.year, 2) + fresh + moci, data = abund_guilds)
+
+year2.guild_fresh_moci <- glm.nb(guild.total ~ poly(study.year, 2) * guild + fresh + moci, data = abund_guilds)
+year2_guild_fresh_moci <- glm.nb(guild.total ~ poly(study.year, 2) + guild + fresh + moci, data = abund_guilds)
+
+guild_aic <- aictab(list(year2_fresh_moci, year2.guild_fresh_moci, year2_guild_fresh_moci), 
+                    c("year2_fresh_moci", "year2.guild_fresh_moci", "year2_guild_fresh_moci")) %>% 
+  data.frame()
 
 
+lrtest(year2.guild_fresh_moci, year2_guild_fresh_moci)
+lrtest(year2_guild_fresh_moci, year2_fresh_moci)
 
 
-# abund ~ B0 + year(B1) + year2(B2) + MOCI(B4) +   
+summary(year2.guild_fresh_moci)
+
+
+guild_newdat <- expand.grid(study.year = distinct(abund_guilds, study.year)$study.year,
+                            guild = distinct(abund_guilds, guild)$guild) %>% 
+  mutate(moci = 0,
+         fresh = 0)
+
+guild_pred = predict(year2_guild_fresh_moci, guild_newdat, type = "link", se = TRUE) %>% 
+  bind_cols(guild_newdat) %>%
+  data.frame() %>% 
+  mutate(lci = exp(fit - (1.96 * se.fit)),
+         uci = exp(fit + (1.96 * se.fit)),
+         predicted = exp(fit))
+
+saveRDS(guild_pred, here("data_files/guild_preds_response"))
+
+year_breaks <- seq(1990, 2025, by = 5)
+
+guild_pred %>% 
+  left_join(abund_guilds %>% dplyr::select(study.year, guild, guild.total)) %>% 
+  full_join(guild_n_spp) %>% 
+  mutate(guild = paste(guild, " (", n.spp, " species)", sep = "")) %>% 
+  ggplot() +  
+  geom_point(aes(x = study.year, y = guild.total)) +
+  geom_line(aes(x = study.year, y = predicted)) +
+  geom_ribbon(aes(x = study.year, ymin = lci, ymax = uci), alpha = 0.5) +
+  scale_x_continuous(breaks = year_breaks, labels = year_breaks) +
+    theme_bw() +
+    labs(x = "Year",
+         y = "Estimated abundance") +
+  facet_wrap(~guild, scales = "free_y", ncol = 2)
+
+ggsave(here("figures_output/guild_trends_facet.png"), width = 7.5)
+
+
+guild_pred %>% 
+  left_join(abund_guilds %>% dplyr::select(study.year, guild, guild.total)) %>% 
+  full_join(guild_n_spp) %>% 
+  mutate(guild = paste(guild, " (", n.spp, " species)", sep = "")) %>% 
+  ggplot() +  
+  geom_point(aes(x = study.year, y = guild.total, color = guild)) +
+  geom_line(aes(x = study.year, y = predicted, color = guild)) +
+  geom_ribbon(aes(x = study.year, ymin = lci, ymax = uci, fill = guild), alpha = 0.5) +
+  scale_x_continuous(breaks = year_breaks, labels = year_breaks) +
+    theme_bw() +
+    labs(x = "Year",
+         y = "Estimated abundance",
+         color = "",
+         fill = "") +
+  theme(legend.position = c(.8, .9),
+        legend.text.align = 0) 
+
+
+ggsave(here("figures_output/guild_trends.png"), width = 7.5)
